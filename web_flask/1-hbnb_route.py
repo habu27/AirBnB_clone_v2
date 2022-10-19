e24805f62eb2e65489955726d4498b01dd2f36e6
habu27 <100352651+habu27@users.noreply.github.com>
2022-10-19 12:35:07 -0700

    task1

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,44 +1,20 @@
 #!/usr/bin/python3
-<<<<<<< HEAD
-"""the `1-hbnb_route` module
-starts a flask web application listening on `0.0.0.0:5000`
-"""
+"""Starts a Flask web application"""
 
 from flask import Flask
-
 app = Flask(__name__)
-app.url_map.strict_slashes = False
 
 
-@app.route("/", strict_slashes=False)
-def index():
-    """returns `Hello HBNB!` message"""
-    return "Hello HBNB!"
-
-
-@app.route("/hbnb", strict_slashes=False)
-def hbnb():
-    """returns `HBNB` message"""
-    return "HBNB"
-
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
-=======
-"""
-starts a Flask web application
-"""
-from flask import Flask
-app = Flask(__name__)
 @app.route('/', strict_slashes=False)
-def index():
-    """returns Hello HBNB!"""
+def Hello HBNB!():
+    """Returns a string at the root route"""
     return 'Hello HBNB!'
+
+
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """returns HBNB"""
+    """Returns a string at the /hbnb route"""
     return 'HBNB'
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
->>>>>>> 212529db4b955a72d996660137790c2497399ff5
+    app.run(host='0.0.0.0')
